6dbf830efe8959e815efe3ffe63c0c66045efa97
yamataso <yamataso0876@gmail.com>
2022-12-22 22:28:28 +0900

    ✨マイグレーションファイルを作成しました

---
+++ database/migrations/2022_12_20_112244_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_information', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image');
+            $table->string('product_name');
+            $table->string('price');
+            $table->string('inventory');
+            $table->string('manufacture_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_information');
+    }
+}
