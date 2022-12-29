f0f3533ad1bb8d2ca8a93e8b291a103801d7b981
yamataso <yamataso0876@gmail.com>
2022-12-29 22:31:51 +0900

    ♻️ Modelに記載したgetHomeを呼んで、Viewに渡すような内容を作成しました。

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
+
 
 class ArticleController extends Controller
 {
-    //
+    public function showHome(){
+        //インスタンス生成
+        $model = new Article();
+        $product_information = $model->getHome();
+        return view('home',compact('product_information'));
+    }
 }
