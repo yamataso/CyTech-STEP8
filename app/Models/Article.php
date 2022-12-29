19f1603a58a7ad4a6bb8e8019c36408f6c8ca562
yamataso <yamataso0876@gmail.com>
2022-12-29 22:23:57 +0900

    ♻️ tableからデータを取得する処理を作成しました

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 class Article extends Model
 {
-    //
+    public function getHome() {
+        // product_informationテーブルからデータを取得
+        $product_information = DB::table('product_information')->get();
+
+        return $product_information;
+    }
 }
