10fcf70224cb8307ca287fdf2d512b6058df8388
yamataso <yamataso0876@gmail.com>
2023-01-26 21:59:25 +0900

    ♻️ テーブル名の修正

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\DB;
 class Article extends Model
 {
     public function getHome() {
-        // product_informationテーブルからデータを取得
-        $product_information = DB::table('product_information')->get();
-
-        return $product_information;
+        // productsテーブルからデータを取得
+        $products = DB::table('products')->get();
+        return $products;
     }
 }
+
