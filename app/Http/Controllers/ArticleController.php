d8ae16514e8b1df1ac48bedabeecb712debce0cf
yamataso <yamataso0876@gmail.com>
2023-01-27 21:42:40 +0900

    ♻️ コントローラーの修正です

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,14 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
-
+use App\Http\Requests\ArticleRequest;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
     public function showHome(){
         //インスタンス生成
         $model = new Article();
-        $product_information = $model->getHome();
-        return view('home',compact('product_information'));
+        $products = $model->getHome();
+        return view('home',compact('products'));
     }
+
+    // public function registSubmit(ArticleRequest $request){
+    //     //トランザクション開始
+    //     DB::beginTransaction();
+
+    //     try {
+    //         // 登録処理呼び出し
+    //         $model = new Article();
+    //         $model->registArticle($request);
+    //         DB::commit();
+    //     } catch (\Exception $e) {
+    //         DB::rollback();
+    //         return back();
+    //     }
+    //     // 処理が完了したらregistにリダイレクト
+    //     return redirect(route('regist'));
+    // }
+    // public function store(Requset $request){
+    //     $model = new Article();
+
+    //     $image_path = $request->file('image')->store('public');
+
+    //     $model->image = basename($image_path);
+
+    //     $model->save();
+
+    //     return view('regist');
+    // }
+    // public function index(){
+    //     $model = Article::all();
+    //     return view('regist', $model);
+    // }
 }
