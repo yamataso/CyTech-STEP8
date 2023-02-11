9e4b62886d624e1f4d5d8d16035d5da5b4d5d1c1
yamataso <yamataso0876@gmail.com>
2023-02-11 21:58:20 +0900

    ♻️ ブレードの修正です。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1>商品一覧画面表示</h1>
+            
             <div class="card">
                 <div class="card-header">検索機能</div>
                     <div class="card-body">
@@ -15,41 +16,45 @@
                     <input type="button" onclick="location.href='./'" value="検索">
                 </div>
             </div>
-
+        
             <div class="card">
                 <div class="card-header">商品情報</div>
                 <div class="card-body">
-                <table class="table">
-                <thead>
-                    <tr>
-                        <th>会社のid</th>
-                        <th>商品画像</th>
-                        <th>商品名</th>
-                        <th>価格</th>
-                        <th>在庫数</th>
-                        <th>メーカー名</th>
-                    </tr>
-                     @foreach($products as $product)
-                <tr>
-                    <td>{{$product->company_id}}</td>
-                    <td>{{$product->img_path}}</td>
-                    <td>{{$product->product_name}}</td>
-                    <td>{{$product->price}}</td>
-                    <td>{{$product->stock}}</td>
-                </tr>
-                    @endforeach
-
-                </thead>
-                <tbody>
-                </div>
-                </tbody>
-                </table>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>会社のid</th>
+                                <th>商品画像</th>
+                                <th>商品名</th>
+                                <th>価格</th>
+                                <th>在庫数</th>
+                                <th>メーカー名</th>
+                            </tr>
+                        <div>
+                            @foreach($products as $product)
+                                <tr>
+                                    <td>{{$product->id}}</td>
+                                    <td>{{$product->img_path}}</td>
+                                    <td>{{$product->product_name}}</td>
+                                    <td>{{$product->price}}</td>
+                                    <td>{{$product->stock}}</td>
+                                    <td>{{$product->company_id}}</td>
+                                    
+                                </tr>
+                                <div class="form-row">
+                                    <input type="button" value="詳細表示">
+                                </div>
+                        </div>
+                            @endforeach
+                        </thead>
+                    </div>
+                    
+                    </table>
             </div>
         </div>
         
-        <input type="button" onclick="location.href='http://localhost:8888/STEP7/public/regist'" value="新規登録">
+        <input type="button" onclick="location.href='http://localhost:8888/STEP7/public/post/create'" value="新規登録">
         <input type="button" value="削除">
-        <input type="button" value="詳細表示">
     </div>
 </div>
 @endsection
--- resources/views/regist.blade.php
+++ resources/views/regist.blade.php
@@ -6,21 +6,19 @@
     <div class="container">
         <h1>商品情報登録画面</h1>
         <div class="row">
-            <form class="form-group" action="{{ route('submit') }}" method="post" enctype="multipart/form-data">
+            <form class="form-group" action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
-
                 <div class="form-group">
-                    <label for="name">商品名</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{old('name')}}">
+                    <label for="product_name">商品名</label>
+                    <input type="text" class="form-control" id="product_name" name="product_name" placeholder="Name">
                     @if($errors->has('name'))
                     <p>{{ $errors->first('name') }}</p>
                     @endif
                 </div>
-
                 <div class="form-group">
                     <label for="company">会社名を選択してください</label>
                    
-                    <select class="form-control" name="company" id="company">
+                    <select class="form-control" name="company_id" id="company">
                     @foreach($companies as $company)
                     <option value="{{$company->id}}">{{$company->company_name}}</option>
                     @endforeach
@@ -31,10 +29,11 @@
                     <p>{{ $errors->first('company') }}</p>
                     @endif
                 </div>
+ 
 
                 <div class="form-group">
                     <label for="price">価格</label>
-                    <input type="text" class="form-control" id="price" name="price" placeholder="Price" value="{{old('price')}}">
+                    <input type="text" class="form-control" id="price" name="price" placeholder="Price" >
                     @if($errors->has('price'))
                     <p>{{ $errors->first('price') }}</p>
                     @endif
@@ -42,7 +41,7 @@
 
                 <div class="form-group">
                     <label for="stock">在庫数</label>
-                    <input type="text" class="form-control" id="stock" name="stock" placeholder="Stock" value="{{old('stock')}}">
+                    <input type="text" class="form-control" id="stock" name="stock" placeholder="Stock" >
                     @if($errors->has('stock'))
                     <p>{{ $errors->first('stock') }}</p>
                     @endif
@@ -50,7 +49,7 @@
 
                 <div class="form-group">
                     <label for="comment">コメント</label>
-                    <textarea class="form-control" id="comment" name="comment" placeholder="Comment">{{ old('comment') }}</textarea>
+                    <textarea class="form-control" id="comment" name="comment" placeholder="Comment"></textarea>
                     @if($errors->has('comment'))
                     <p>{{ $errors->first('comment') }}</p>
                     @endif
@@ -58,13 +57,12 @@
 
                 <div class="form-group">
                     <label for="title">商品画像</label>
-                    <input type="file" class="form-control" id="image" name="image" placeholder="Image" accept="image/*">
+                    <input type="file" class="form-control" id="img_path" name="img_path" placeholder="Image" >
                 </div>
 
                 <button type="submit" class="btn">登録</button>
-                <button type="submit" class="btn" onclick="location.href='http://localhost:8888/STEP7/public/home'"  >戻る</button>
-
             </form>
         </div>
+        <button type="submit" class="btn" onclick="location.href='http://localhost:8888/STEP7/public/home'"  >戻る</button>
     </div>
 @endsection
